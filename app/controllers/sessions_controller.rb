0c36391a3da4c2b5d84e94f6e7a5498affeff800
Klinsmann Acosta <klinsmann17@gmail.com>
2020-02-12 14:57:26 -0600

    Update session routes, render using jbuilder

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,15 @@
 class SessionsController < ApplicationController
-  before_action :set_session, only: [:show]
-
-  def index
-    @sessions = Session.all
-
-    render json: @sessions
-  end
-
-  def show
-    render json: @session
-  end
-
   def create
     @session = Session.new(session_params)
 
     if @session.save
-      render json: @session, status: :created, location: @session
+      render json: @session, status: :created
     else
-      render json: @session.errors, status: :unprocessable_entity
+      render_error error: @session.errors, status: :unprocessable_entity
     end
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_session
-    @session = Session.find(params[:id])
-  end
 
   # Only allow a trusted parameter "white list" through.
   def session_params
